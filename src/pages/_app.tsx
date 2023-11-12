fb17626cb684055773c40a0657027349ef9b09d6
naomi <naomina@me.com>
2023-11-13 00:37:25 +0900

    不要な機能の削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
 import { ToastContainer, toast } from 'react-toastify'
 import Loader from '@/components/Loder'
 import { ConfirmProvider } from '@/context/confirm-provider'
@@ -10,11 +9,9 @@ export default function App({ session, Component, pageProps }: any & AppProps) {
     <>
       <Loader />
       <ToastContainer />
-      <SessionProvider session={session}>
         <ConfirmProvider>
           <Component {...pageProps} />
         </ConfirmProvider>
-      </SessionProvider>
     </>
   )
 }
